d6dadba6b89f11fea3386fa6361b7ffb1d312e04
Aryan Kapadiya <aryankapadiya2502@gmail.com>
2026-04-11 20:49:47 +0530

    Add Demo class with two methods

---
+++ Unit-3/Program1.py
@@ -0,0 +1,10 @@
+class Demo:
+    def show_message(self):
+        print("Hello from first method")
+
+    def show_number(self):
+        print("This is second method")
+
+obj = Demo()
+obj.show_message()
+obj.show_number()
